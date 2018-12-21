2847166917ee4c17d2e05b0696b64e10316d5fb3
shyakaC <shyakachaffi@gmail.com>
2018-12-21 11:39:43 +0100

    ajout et test boostrap dur la view_index

--- view/view_index.php
+++ view/view_index.php
@@ -6,7 +6,8 @@
         <title>Welcome to MSN (My Social Network)!</title>
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <!--<link href="css/style.css" rel="stylesheet" type="text/css"/>-->
+         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <div class="title">Welcome to MSN (My Social Network)!</div>
--- view/view_login.php
+++ view/view_login.php
@@ -7,16 +7,27 @@
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
     </head>
-    <body>
-        <div class="title">Log In</div>
-        <div class="menu">
-            <a href="main/index">Home</a>
-            <a href="main/signup">Sign Up</a>
+    <body class="container">
+        <div class="title" class="container-fluid" class="h1">Log In</div>
+        <nav class="navbar-form navbar-right inline-form">
+        <div class="menu"  >
+            <a href="main/index" class="nav-link active">Home</a>
+            <a href="main/signup"  class="nav-link active">Sign Up</a>
+            </nav>
         </div>
-        <div class="main">
+        <div class="main"  class="navbar-form navbar-right inline-form">
             <form action="main/login" method="post">
-                
+
                 <table>
                     <tr>
                         <td>Pseudo:</td>
@@ -27,7 +38,7 @@
                         <td><input id="password" name="password" type="password" value="<?= $password ?>"></td>
                     </tr>
                 </table>
-                <input type="submit" value="Log In">
+                <button type="submit" value="log in"   class="btn btn-light"><span   class="glyphicon glyphicon-eye-open">Login</span></button>
             </form>
             <?php if (count($errors) != 0): ?>
                 <div class='errors'>
