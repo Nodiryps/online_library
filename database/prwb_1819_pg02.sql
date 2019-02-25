d28cec797918b11fa373d22bc3c728e2eaca1c03
2707chshyaka <2707chshyaka@PC9846.domepfc.net>
2019-02-25 19:43:22 +0100

    Ajout de la nouvelle base de données avec colonne nbCopies

--- database/prwb_1819_pg02.sql
+++ database/prwb_1819_pg02.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 16, 2018 at 02:47 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Hôte : localhost
+-- Généré le :  lun. 25 fév. 2019 à 18:41
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,92 +19,141 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prwb_1819_pc06g02`
+-- Base de données :  `prwb_1819_pg02`
 --
+
+-- --------------------------------------------------------
 DROP DATABASE IF EXISTS `prwb_1819_pg02`;
 CREATE DATABASE IF NOT EXISTS `prwb_1819_pg02` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `prwb_1819_pg02`;
-
--- --------------------------------------------------------
-
 --
--- Table structure for table `book`
+-- Structure de la table `book`
 --
 
-DROP TABLE IF EXISTS `book`;
-CREATE TABLE IF NOT EXISTS `book` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
   `isbn` char(13) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `editor` varchar(255) NOT NULL,
   `picture` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `isbn_UNIQUE` (`isbn`)
+  `nbCopies` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `book`
+--
+
+INSERT INTO `book` (`id`, `isbn`, `title`, `author`, `editor`, `picture`, `nbCopies`) VALUES
+(1, '1111111111111', 'Harrys Potter à l Ecole des Sorciers', 'JK Rowlings', 'Pocket', 'Harrys Potter à l Ecole des Sorciers.jpg', 1),
+(2, '2222222222222', 'Mobyz Dick', 'Jules Verges', 'Pocket', 'Mobyz Dick.jpg', 1),
+(3, '3333333333333', 'Les Fleurs du Male', 'Beau De Lair', 'Pockets', 'Les Fleurs du Male.jpg', 1),
+(4, '4444444444444', 'Madame Bovary', 'Gustave Flaubert', 'Pocket', 'Madame Bovary.jpg', 1),
+(5, '5555555555555', 'Peter Pan', 'Linus Torvalds', 'Pocket', 'Peter Pan.jpg', 1),
+(6, '6666666666666', 'A Brief History of Time', 'Stephen Hawking', 'Pocket', NULL, 1),
+(7, '7777777777777', '1984', 'George Orwell', 'Pocket', NULL, 1),
+(8, '8888888888888', 'To Kill a Mockingbird', 'Harper Lee', 'Pocket', 'To Kill a Mockingbird.jpg', 1),
+(9, '9999999999999', 'Lord of the Flies', 'William Goldings', 'Pocket', NULL, 1),
+(10, '1111111111110', 'La Danse de la Colere', 'Lerner', 'Pocket', 'La Danse de la Colere.jpg', 1),
+(11, '2222222222220', 'Why Does He Do That', 'Lundy Bancroft', 'Pocket', NULL, 1),
+(12, '3333333333330', 'The Stranger', 'Albert Camus', 'Pocket', 'The Stranger.jpg', 1),
+(13, '4444444444440', 'Heart of Darkness', 'Joseph Conrad', 'Pocket', NULL, 1),
+(14, '5555555555550', 'Men Without Women', 'Ernest Hemingway', 'Pocket', NULL, 1);
+
 -- --------------------------------------------------------
-INSERT INTO `book` (`id`, `isbn`, `title`, `author`, `editor`, `picture`) VALUES
-(1, '1111111111111', 'Harrys Potter à l Ecole des Sorciers', 'JK Rowlings', 'Pocket', 'Harrys Potter à l Ecole des Sorciers.jpg'),
-(2, '2222222222222', 'Mobyz Dick', 'Jules Verges', 'Pocket', 'Mobyz Dick.jpg'),
-(3, '3333333333333', 'Les Fleurs du Male', 'Beau De Lair', 'Pockets', 'Les Fleurs du Male.jpg'),
-(4, '4444444444444', 'Madame Bovary', 'Gustave Flaubert', 'Pocket', 'Madame Bovary.jpg'),
-(5, '5555555555555', 'Peter Pan', 'Linus Torvalds', 'Pocket', 'Peter Pan.jpg'),
-(6, '6666666666666', 'A Brief History of Time', 'Stephen Hawking', 'Pocket', NULL),
-(7, '7777777777777', '1984', 'George Orwell', 'Pocket', NULL),
-(8, '8888888888888', 'To Kill a Mockingbird', 'Harper Lee', 'Pocket', 'To Kill a Mockingbird.jpg'),
-(9, '9999999999999', 'Lord of the Flies', 'William Goldings', 'Pocket', NULL),
-(10, '1111111111110', 'La Danse de la Colere', 'Lerner', 'Pocket', 'La Danse de la Colere.jpg'),
-(11, '2222222222220', 'Why Does He Do That', 'Lundy Bancroft', 'Pocket', NULL),
-(12, '3333333333330', 'The Stranger', 'Albert Camus', 'Pocket', 'The Stranger.jpg'),
-(13, '4444444444440', 'Heart of Darkness', 'Joseph Conrad', 'Pocket', NULL),
-(14, '5555555555550', 'Men Without Women', 'Ernest Hemingway', 'Pocket', NULL);
 
 --
--- Table structure for table `rental`
+-- Structure de la table `rental`
 --
 
-DROP TABLE IF EXISTS `rental`;
-CREATE TABLE IF NOT EXISTS `rental` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rental` (
+  `id` int(11) NOT NULL,
   `user` int(11) NOT NULL,
   `book` int(11) NOT NULL,
   `rentaldate` datetime DEFAULT NULL,
-  `returndate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_rentalitem_book1_idx` (`book`),
-  KEY `fk_rentalitem_user1_idx` (`user`)
+  `returndate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(255) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `email` varchar(64) NOT NULL,
   `birthdate` date DEFAULT NULL,
-  `role` enum('admin','manager','member') NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username_unique` (`username`) USING BTREE,
-  UNIQUE KEY `email_unique` (`email`)
+  `role` enum('admin','manager','member') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Constraints for dumped tables
+-- Déchargement des données de la table `user`
 --
-INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `email`, `birthdate`, `role`) VALUES 
-(NULL, 'admin', '903001ff9a17773d4a0b4cff3666f1e9', 'chaffi', 'admin@epfc.com', NULL, 'admin'), 
-(NULL, 'manager', '903001ff9a17773d4a0b4cff3666f1e9', 'Spyridon', 'manager@epfc.com', NULL, 'manager'), 
-(NULL, 'member', '903001ff9a17773d4a0b4cff3666f1e9', 'virginie', 'member@epfc.com', NULL, 'member');
+
+INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `email`, `birthdate`, `role`) VALUES
+(1, 'admin', '903001ff9a17773d4a0b4cff3666f1e9', 'chaffi', 'admin@epfc.com', NULL, 'admin'),
+(2, 'manager', '903001ff9a17773d4a0b4cff3666f1e9', 'Spyridon', 'manager@epfc.com', NULL, 'manager'),
+(3, 'member', '903001ff9a17773d4a0b4cff3666f1e9', 'virginie', 'member@epfc.com', NULL, 'member');
+
 --
--- Constraints for table `rental`
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `isbn_UNIQUE` (`isbn`);
+
+--
+-- Index pour la table `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_rentalitem_book1_idx` (`book`),
+  ADD KEY `fk_rentalitem_user1_idx` (`user`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_unique` (`username`) USING BTREE,
+  ADD UNIQUE KEY `email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT pour la table `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `rental`
 --
 ALTER TABLE `rental`
   ADD CONSTRAINT `fk_rentalitem_book` FOREIGN KEY (`book`) REFERENCES `book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
