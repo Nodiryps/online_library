35626fbfca2d17f429f1215233563d21d3240cd5
2707chshyaka <2707chshyaka@PC9655.domepfc.net>
2019-05-13 20:21:35 +0200

    really done ISBN ;-p

--- js/newBook.js
+++ js/newBook.js
@@ -1,6 +1,6 @@
 
 $(function () {
-     $('#ISBN').after(isbn2);
+    $('#ISBN').after(isbn2);
     isbn();
     addFeatures();
     onFocus();
@@ -48,16 +48,20 @@ function addFeatures() {
 }
 
 function onFocus() {
+    $('#isbn2')
 
     $('#ISBN').focusin(function () {
-        $('#ISBN').after(isbn2);
+        if ($('#isbn2').length === 0) {
+            $('#ISBN').after(isbn2);
+        }
+
         $('#ISBN').removeClass("form-control input-md");
         $('#ISBN').val($('#ISBN').val().replace(/\-/g, ''));
         if ($('#ISBN').val().length === 13) {
             $('#isbn2').val($('#ISBN').val().substr(-1));
-             $('#ISBN').val($('#ISBN').val().substr(0, $('#ISBN').val().length - 1))
-            
-            
+            $('#ISBN').val($('#ISBN').val().substr(0, $('#ISBN').val().length - 1))
+
+
         }
 
     });
